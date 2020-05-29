808e1f9111d719b648f1e8b5a81324acbaac6c0b
maidiance <winnie.lu@comcast.net>
2020-05-28 22:40:49 -0700

    Birthday Card submission

---
+++ BirthdayCard/BirthdayCard/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirthdayCard.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SendCard(Models.GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("SendCard", guestResponse);
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ BirthdayCard/BirthdayCard/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BirthdayCard
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                // Add FULL MVC handling (Do not call AddMvcCore())
+                .AddMvc()
+                // Be explicit in what version MVC should be using
+                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles(); // Add this
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
